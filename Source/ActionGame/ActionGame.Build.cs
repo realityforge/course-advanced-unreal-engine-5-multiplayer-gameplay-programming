07645913beba67b6b86ca9ff82ff292ef1606706
Peter Donald <peter@realityforge.org>
2024-10-05 21:32:41 +1000

    Add modules for Gameplay plugin

--- Source/ActionGame/ActionGame.Build.cs
+++ Source/ActionGame/ActionGame.Build.cs
@@ -8,7 +8,10 @@ public class ActionGame : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(
-            new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+        // Make sure we discover files from module directory
+        PublicIncludePaths.Add("ActionGame/");
+
+        PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+        PrivateDependencyModuleNames.AddRange(new[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
     }
 }
