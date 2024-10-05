59ad4641f1ea95236099d5b87f901557e2a9e989
Peter Donald <peter@realityforge.org>
2024-10-05 21:47:00 +1000

    Init AbailitySystemComponent

---
+++ Source/ActionGame/AbilitySystem/Components/AG_AbilitySystemComponent.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AbilitySystemComponent.h"
+#include "CoreMinimal.h"
+#include "AG_AbilitySystemComponent.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ACTIONGAME_API UAG_AbilitySystemComponent : public UAbilitySystemComponent
+{
+    GENERATED_BODY()
+};
