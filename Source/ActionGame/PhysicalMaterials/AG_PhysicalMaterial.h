ab875061a9244071ab0ab48d7149973905feae4f
Peter Donald <peter@realityforge.org>
2024-10-10 09:30:56 +1100

    Expose FootstepSound

--- Source/ActionGame/PhysicalMaterials/AG_PhysicalMaterial.h
+++ Source/ActionGame/PhysicalMaterials/AG_PhysicalMaterial.h
@@ -10,4 +10,7 @@ class ACTIONGAME_API UAG_PhysicalMaterial : public UPhysicalMaterial
     GENERATED_BODY()
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physical Material", meta = (AllowPrivateAccess = "true"))
     TObjectPtr<USoundBase> FootstepSound{ nullptr };
+
+public:
+    FORCEINLINE USoundBase* GetFootstepSound() const { return FootstepSound; }
 };
