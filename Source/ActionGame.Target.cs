1826e15853265fa88a725d5cad20d0129cd15806
Peter Donald <peter@realityforge.org>
2025-01-21 20:15:19 +1100

    Move to the latest build settings

--- Source/ActionGame.Target.cs
+++ Source/ActionGame.Target.cs
@@ -6,8 +6,8 @@ public class ActionGameTarget : TargetRules
     public ActionGameTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("ActionGame");
     }
 }
