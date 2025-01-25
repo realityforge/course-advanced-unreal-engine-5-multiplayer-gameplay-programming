ed15403a37eb9f681d978964f802b675d2927af8
Peter Donald <peter@realityforge.org>
2025-01-25 15:07:50 +1100

    Cleanup the braidup tasks

--- bin/braidup.bat
+++ bin/braidup.bat
@@ -13,4 +13,4 @@ rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem See the License for the specific language governing permissions and
 rem limitations under the License.
 
-braid push Plugins/RuleRanger && git add . && git stash && braid up Plugins/RuleRanger && git stash pop && git reset
+braid push Plugins/RuleRanger && braid push Plugins/ModularGameplayActors && braid push Plugins/ModularGasGameplayActors && git add . && git stash && braid up Plugins/Aeon && braid up Plugins/RuleRanger && braid up Plugins/ModularGameplayActors && braid up Plugins/ModularGasGameplayActors && git stash pop && git reset
