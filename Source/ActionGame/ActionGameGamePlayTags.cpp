dba7bf86d2b2bf2de7127c4cd1334d2bb9d0d040
Peter Donald <peter@realityforge.org>
2025-02-10 11:32:37 +1100

    Define some tags that will come into play

---
+++ Source/ActionGame/ActionGameGamePlayTags.cpp
@@ -0,0 +1,10 @@
+#include "ActionGameGameplayTags.h"
+
+namespace ActionGameGameplayTags
+{
+    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_EquipItemActor, "Event.Inventory.EquipItemActor")
+    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_DropItem, "Event.Inventory.DropItem")
+    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_EquipNext, "Event.Inventory.EquipNext")
+    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_Unequip, "Event.Inventory.Unequip")
+
+} // namespace ActionGameGameplayTags
---
+++ Source/ActionGame/ActionGameGamePlayTags.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace ActionGameGameplayTags
+{
+
+    ACTIONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Inventory_EquipItemActor)
+    ACTIONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Inventory_DropItem)
+    ACTIONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Inventory_EquipNext)
+    ACTIONGAME_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Event_Inventory_Unequip)
+
+} // namespace ActionGameGameplayTags
