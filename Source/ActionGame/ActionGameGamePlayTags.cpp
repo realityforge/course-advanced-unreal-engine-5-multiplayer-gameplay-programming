fc1253b280804ae041166524c752265bf554d661
Peter Donald <peter@realityforge.org>
2025-02-23 22:15:08 +1100

    Add comments to describe tags

--- Source/ActionGame/ActionGameGamePlayTags.cpp
+++ Source/ActionGame/ActionGameGamePlayTags.cpp
@@ -2,9 +2,13 @@
 
 namespace ActionGameGameplayTags
 {
-    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_EquipItemActor, "Event.Inventory.EquipItemActor")
-    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_DropItem, "Event.Inventory.DropItem")
-    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_EquipNext, "Event.Inventory.EquipNext")
-    UE_DEFINE_GAMEPLAY_TAG(Event_Inventory_Unequip, "Event.Inventory.Unequip")
+    UE_DEFINE_GAMEPLAY_TAG_COMMENT(Event_Inventory_EquipItemActor,
+                                   "Event.Inventory.EquipItemActor",
+                                   "Equip Item from ItemActor Event")
+    UE_DEFINE_GAMEPLAY_TAG_COMMENT(Event_Inventory_DropItem, "Event.Inventory.DropItem", "Drop current item Event")
+    UE_DEFINE_GAMEPLAY_TAG_COMMENT(Event_Inventory_EquipNext,
+                                   "Event.Inventory.EquipNext",
+                                   "Try and Equip Next Item Event")
+    UE_DEFINE_GAMEPLAY_TAG_COMMENT(Event_Inventory_Unequip, "Event.Inventory.Unequip", "Unequip current item Event")
 
 } // namespace ActionGameGameplayTags
