68a29510e87ff627384c4b16fe175f90657d71f0
Peter Donald <peter@realityforge.org>
2025-03-04 10:08:23 +1100

    Add an abstract base class for Inventory  based abilities

---
+++ Source/ActionGame/AbilitySystem/Abilities/GameplayAbility_InventoryAbility.cpp
@@ -0,0 +1,74 @@
+#include "AbilitySystem/Abilities/GameplayAbility_InventoryAbility.h"
+#include "ActionGameTypes.h"
+#include "Inventory/InventoryComponent.h"
+#include "Inventory/InventoryItemInstance.h"
+#include "Inventory/ItemActors/WeaponItemActor.h"
+#include "Logging/StructuredLog.h"
+
+void UGameplayAbility_InventoryAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo,
+                                                      const FGameplayAbilitySpec& Spec)
+{
+    Super::OnGiveAbility(ActorInfo, Spec);
+
+    InventoryComponent = ActorInfo->OwnerActor.Get()->FindComponentByClass<UInventoryComponent>();
+    if (!InventoryComponent)
+    {
+        UE_LOGFMT(LogTemp,
+                  Error,
+                  "UGameplayAbility_InventoryAbility::OnGiveAbility: Unable to locate InventoryComponent on actor {Actor}",
+                  GetNameSafe(ActorInfo->OwnerActor.Get()));
+    }
+}
+
+UInventoryComponent* UGameplayAbility_InventoryAbility::GetInventoryComponent() const
+{
+    return InventoryComponent;
+}
+
+UInventoryItemInstance* UGameplayAbility_InventoryAbility::GetEquippedItemInstance() const
+{
+    if (InventoryComponent)
+    {
+        const auto InventoryItemInstance = InventoryComponent->GetCurrentItem();
+        if (!InventoryItemInstance)
+        {
+            UE_LOGFMT(LogTemp,
+                      Error,
+                      "UGameplayAbility_InventoryAbility::GetEquippedItemInstance() No CurrentItem on {Actor}",
+                      GetNameSafe(GetActorInfo().OwnerActor.Get()));
+        }
+        return InventoryItemInstance;
+    }
+    else
+    {
+        UE_LOGFMT(LogTemp,
+                  Error,
+                  "UGameplayAbility_InventoryAbility::GetEquippedItemInstance() No InventoryComponent for {Actor}",
+                  GetNameSafe(GetActorInfo().OwnerActor.Get()));
+        return nullptr;
+    }
+}
+
+const UItemStaticData* UGameplayAbility_InventoryAbility::GetEquippedItemStaticData() const
+{
+    const auto ItemInstance = GetEquippedItemInstance();
+    return ItemInstance ? ItemInstance->GetItemStaticData() : nullptr;
+}
+
+const UWeaponStaticData* UGameplayAbility_InventoryAbility::GetEquippedItemWeaponStaticData() const
+{
+    const UItemStaticData* Src = GetEquippedItemStaticData();
+    return Cast<UWeaponStaticData>(Src);
+}
+
+AItemActor* UGameplayAbility_InventoryAbility::GetEquippedItemActor() const
+{
+    const auto ItemInstance = GetEquippedItemInstance();
+    return ItemInstance ? ItemInstance->GetItemActor() : nullptr;
+}
+
+AWeaponItemActor* UGameplayAbility_InventoryAbility::GetEquippedWeaponItemActor() const
+{
+    AItemActor* Src = GetEquippedItemActor();
+    return Cast<AWeaponItemActor>(Src);
+}
---
+++ Source/ActionGame/AbilitySystem/Abilities/GameplayAbility_InventoryAbility.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "AbilitySystem/Abilities/AG_GameplayAbility.h"
+#include "CoreMinimal.h"
+#include "GameplayAbility_InventoryAbility.generated.h"
+
+class UInventoryItemInstance;
+class UItemStaticData;
+class UWeaponStaticData;
+class AItemActor;
+class AWeaponItemActor;
+class UInventoryComponent;
+
+UCLASS(Abstract)
+class ACTIONGAME_API UGameplayAbility_InventoryAbility : public UAG_GameplayAbility
+{
+    GENERATED_BODY()
+
+    /** Cached copy of Inventory Component. */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess=true))
+    TObjectPtr<UInventoryComponent> InventoryComponent{ nullptr };
+
+public:
+    virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure)
+    UInventoryComponent* GetInventoryComponent() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure)
+    UInventoryItemInstance* GetEquippedItemInstance() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure)
+    const UItemStaticData* GetEquippedItemStaticData() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure)
+    const UWeaponStaticData* GetEquippedItemWeaponStaticData() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure)
+    AItemActor* GetEquippedItemActor() const;
+
+    UFUNCTION(BlueprintCallable, BlueprintPure)
+    AWeaponItemActor* GetEquippedWeaponItemActor() const;
+};
