171b5b33473ab16a0c3e2c3ed1173279be9e96d7
Peter Donald <peter@realityforge.org>
2025-09-19 22:51:01 +1000

    Backport changes from Aura

--- bin/braidup.sh
+++ bin/braidup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-braid push Plugins/RuleRanger && braid push Plugins/ModularGameplayActors && braid push Plugins/ModularGasGameplayActors && git add . && git stash && braid up Plugins/Aeon && braid up Plugins/RuleRanger && braid up Plugins/ModularGameplayActors && braid up Plugins/ModularGasGameplayActors && git stash pop && git reset
+braid push Plugins/Aeon && braid push Plugins/ModularGameplayActors && braid push Plugins/ModularGasGameplayActors && braid push Plugins/RuleRanger && git add . && git stash && braid up Plugins/Aeon && braid up Plugins/ModularGameplayActors && braid up Plugins/ModularGasGameplayActors && braid up Plugins/RuleRanger && braid up Plugins/MDMetaDataEditor && git stash pop && git reset
