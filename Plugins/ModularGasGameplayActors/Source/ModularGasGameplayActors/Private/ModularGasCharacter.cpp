b72f18d942ea9ef1ba459b7c3fc87ecb8a0b690d
Peter Donald <peter@realityforge.org>
2025-09-19 22:48:40 +1000

    Braid: Update mirror 'Plugins/ModularGasGameplayActors' to 'bc7463e'

--- Plugins/ModularGasGameplayActors/Source/ModularGasGameplayActors/Private/ModularGasCharacter.cpp
+++ Plugins/ModularGasGameplayActors/Source/ModularGasGameplayActors/Private/ModularGasCharacter.cpp
@@ -13,7 +13,7 @@
  */
 #include "ModularGasCharacter.h"
 #include "AbilitySystemComponent.h"
-#include "Components/GameFrameworkComponentManager.h"
+#include "GameFramework/PlayerState.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(ModularGasCharacter)
 
@@ -21,7 +21,107 @@ const FName AModularGasCharacter::NAME_AbilitySystemComponent("AbilitySystemComp
 
 AModularGasCharacter::AModularGasCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
-    AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(NAME_AbilitySystemComponent);
+    // The subclass should invoke ObjectInitializer.DoNotCreateDefaultSubobject(NAME_AbilitySystemComponent) if
+    // PlayerStateOwned == OwnerPolicy
+    AbilitySystemComponent = CreateOptionalDefaultSubobject<UAbilitySystemComponent>(NAME_AbilitySystemComponent);
+}
+
+void AModularGasCharacter::InitAbilityActorInfo()
+{
+    if (ensureAlwaysMsgf(AbilitySystemComponent,
+                         TEXT("On actor %s InitAbilityActorInfo() invoked but "
+                              "AbilitySystemComponent not configured."),
+                         *GetActorNameOrLabel()))
+    {
+        if (EAbilitySystemComponentOwnerPolicy::CharacterOwned == OwnerPolicy)
+        {
+            AbilitySystemComponent->InitAbilityActorInfo(this, this);
+            ConfigureAbilitySystemComponent();
+        }
+        else
+        {
+            // ReSharper disable once CppTooWideScopeInitStatement
+            const auto PS = GetPlayerState();
+            if (ensureAlwaysMsgf(PS,
+                                 TEXT("On actor %s the InitAbilityActorInfo() method was invoked with "
+                                      "OwnerPolicy=PlayerStateOwned but PlayerState is not present. "
+                                      "Is the Character Possessed?"),
+                                 *GetActorNameOrLabel()))
+            {
+                AbilitySystemComponent->InitAbilityActorInfo(PS, this);
+                ConfigureAbilitySystemComponent();
+            }
+        }
+    }
+}
+
+void AModularGasCharacter::ConfigureAbilitySystemComponent() {}
+
+void AModularGasCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+    if (EAbilitySystemComponentSetupPolicy::BeginPlay == SetupPolicy)
+    {
+        // Init AbilityActorInfo on the Client and the Server
+        InitAbilityActorInfo();
+    }
+}
+
+void AModularGasCharacter::PossessedBy(AController* NewController)
+{
+    Super::PossessedBy(NewController);
+    if (HasAuthority())
+    {
+        if (EAbilitySystemComponentSetupPolicy::OnPossess == SetupPolicy)
+        {
+            if (EAbilitySystemComponentOwnerPolicy::PlayerStateOwned == OwnerPolicy)
+            {
+                // ReSharper disable once CppTooWideScopeInitStatement
+                const auto AscPlayerState = GetPlayerState<IAbilitySystemInterface>();
+                if (ensureAlwaysMsgf(
+                        AscPlayerState,
+                        TEXT("On actor %s SetupPolicy=OnPossess and OwnerPolicy=PlayerStateOwned "
+                             "but the PlayerState object %s () does not implement IAbilitySystemInterface."),
+                        *GetActorNameOrLabel(),
+                        *GetNameSafe(GetPlayerState())))
+                {
+                    AbilitySystemComponent = AscPlayerState->GetAbilitySystemComponent();
+                    check(AbilitySystemComponent);
+                }
+            }
+
+            // Init AbilityActorInfo on the Server
+            InitAbilityActorInfo();
+        }
+    }
+}
+
+void AModularGasCharacter::OnRep_PlayerState()
+{
+    Super::OnRep_PlayerState();
+
+    checkf(!HasAuthority(),
+           TEXT("Assuming OnRep_PlayerState() is only called on client and thus ASC will never be doubly setup"));
+    if (EAbilitySystemComponentSetupPolicy::OnPossess == SetupPolicy)
+    {
+        if (EAbilitySystemComponentOwnerPolicy::PlayerStateOwned == OwnerPolicy)
+        {
+            // ReSharper disable once CppTooWideScopeInitStatement
+            const auto AscPlayerState = GetPlayerState<IAbilitySystemInterface>();
+            if (ensureAlwaysMsgf(AscPlayerState,
+                                 TEXT("On actor %s SetupPolicy=OnPossess and OwnerPolicy=PlayerStateOwned "
+                                      "but the PlayerState object %s () does not implement IAbilitySystemInterface."),
+                                 *GetActorNameOrLabel(),
+                                 *GetNameSafe(GetPlayerState())))
+            {
+                AbilitySystemComponent = AscPlayerState->GetAbilitySystemComponent();
+                check(AbilitySystemComponent);
+            }
+        }
+
+        // Init AbilityActorInfo on the Server
+        InitAbilityActorInfo();
+    }
 }
 
 UAbilitySystemComponent* AModularGasCharacter::GetAbilitySystemComponent() const
