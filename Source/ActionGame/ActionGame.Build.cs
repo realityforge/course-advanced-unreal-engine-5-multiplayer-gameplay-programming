4c2a341b779ef3999eb6bcce8d0f3532c4a3ad3c
Peter Donald <peter@realityforge.org>
2025-09-20 07:48:33 +1000

    Cleanup path

--- Source/ActionGame/ActionGame.Build.cs
+++ Source/ActionGame/ActionGame.Build.cs
@@ -7,7 +7,7 @@ public class ActionGame : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         // Make sure we discover files from module directory
-        PublicIncludePaths.Add("ActionGame/");
+        PublicIncludePaths.Add("ActionGame");
 
         PublicDependencyModuleNames.AddRange(
             new[] { "Core", "CoreUObject", "Engine", "NetCore", "InputCore", "EnhancedInput", "MotionWarping" });
